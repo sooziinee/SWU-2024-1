1b1b4e934184d66b533911f0303115402e150e21
sooziinee <hsjin777@naver.com>
2024-04-08 16:14:57 +0900

    4/3

---
+++ my_folder/homework/10871.py
@@ -0,0 +1,6 @@
+N, X = list(map(int, input().split()))
+data = list(map(int, input().split()))
+
+for i in data:
+    if i < X:
+        print(i, end="")
---
+++ my_folder/homework/14581.py
@@ -0,0 +1,5 @@
+ID = input()
+
+print(":fan::fan::fan:")
+print(f":fan::{ID}::fan:")
+print(":fan::fan::fan:")
